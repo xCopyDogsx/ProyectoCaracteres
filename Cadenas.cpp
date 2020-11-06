3524d7ec3e0cbde3b1aa247a46a82b039b201cb9
Brian Canon <canonbrian123@gmail.com>
2020-11-06 09:14:02 -0500

    Definicion de objeto y metodos

--- Cadenas.cpp
+++ Cadenas.cpp
@@ -1,1 +1,38 @@
 #include "Cadenas.h"
+
+	void Cadenas::setCadena(char * cadenaX){
+			cadena=cadenaX;
+					}
+	char * Cadenas::getCadena()
+			{
+			return cadena;
+					}
+   Cadenas::Cadenas(char * cadw){
+		setCadena(cadw);
+	}
+void Cadenas::Entrada(){
+	ifstream entrada("texto301");
+	char * cad;
+	while(!entrada.eof()&&entrada>>cad){
+				  Cadenas x(cad);	
+		         }
+}
+		char * Cadenas::ContadorLetras(char * cad){
+			
+		}
+		char * Cadenas::ContadorEspacios (char * cad){
+			
+		}
+		char * Cadenas::ContadorMinus(char * cad){
+			
+		}
+		char * Cadenas::ContadorMayus(char * cad){
+			
+		}
+		char * Cadenas::ContadorCadena(char * cad){
+			
+		}
+		void Cadenas::Salida(){
+			
+		}
+	
--- Cadenas.h
+++ Cadenas.h
@@ -1,11 +1,13 @@
 #ifndef CADENAS_H
 #define CADENAS_H
 #include <bits/stdc++.h>
+using std::ifstream;
+using std::ofstream;
 using namespace std;
 class Cadenas
 {
 	private:
-		char * cadena;
+		char  * cadena;
 	public:
 		void Entrada();
 		char * ContadorLetras(char *);
@@ -14,7 +16,9 @@ class Cadenas
 		char * ContadorMayus(char *);
 		char * ContadorCadena(char *);
 		void Salida();
-
+		void setCadena( char *);
+		char * getCadena();
+		Cadenas(char *); 
 };
 
 #endif
